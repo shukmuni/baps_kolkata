226f622ec34e5787202933381fe53850b9d44271
Yash Patel <yrp.baps@gmail.com>
2021-12-23 11:17:08 +0530

    stable_v3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8c4dcc69afb12bfd85a3f3ff484dab9e",
-"/": "8c4dcc69afb12bfd85a3f3ff484dab9e",
-"main.dart.js": "0ed7335897228bf1d8ce8a635c2c7e86",
+"index.html": "40659b7b6da45ede2f3fe1cecf595509",
+"/": "40659b7b6da45ede2f3fe1cecf595509",
+"main.dart.js": "1f3216d7054b5ae6ae8f1db93aa1818c",
 "manifest.json": "d68ccbbb11c8343c938ae754c440e579",
 "version.json": "8a9e88f32b2188c49092c92ad6d26ab6"
 };
